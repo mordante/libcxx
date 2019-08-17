5b190c940150817a5108dda60287d8e390227032
Mark de Wever <koraq@xs4all.nl>
2019-08-17 14:38:38 +0200

    Remove string_view_type from traits.
    This was suggested by J Wakely.

--- test/std/re/re.traits/types.pass.cpp
+++ test/std/re/re.traits/types.pass.cpp
@@ -15,7 +15,6 @@
 // public:
 //     typedef charT                        char_type;
 //     typedef basic_string<char_type>      string_type;
-//     typedef basic_string_view<char_type> string_view_type;
 //     typedef locale                       locale_type;
 
 #include <regex>
@@ -26,15 +25,9 @@ int main(int, char**)
 {
     static_assert((std::is_same<std::regex_traits<char>::char_type, char>::value), "");
     static_assert((std::is_same<std::regex_traits<char>::string_type, std::string>::value), "");
-#if defined(__cpp_lib_string_view_regex)
-    static_assert((std::is_same<std::regex_traits<char>::string_view_type, std::string_view>::value), "");
-#endif
     static_assert((std::is_same<std::regex_traits<char>::locale_type, std::locale>::value), "");
     static_assert((std::is_same<std::regex_traits<wchar_t>::char_type, wchar_t>::value), "");
     static_assert((std::is_same<std::regex_traits<wchar_t>::string_type, std::wstring>::value), "");
-#if defined(__cpp_lib_string_view_regex)
-    static_assert((std::is_same<std::regex_traits<wchar_t>::string_view_type, std::wstring_view>::value), "");
-#endif
     static_assert((std::is_same<std::regex_traits<wchar_t>::locale_type, std::locale>::value), "");
 
   return 0;
